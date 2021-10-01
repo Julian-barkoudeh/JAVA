51e04347949d11d805a3ff87a0b2e7fdc83460e5
Julian <julian.barkoudeh@gmail.com>
2021-10-01 16:29:32 +0200

    TD1 Exercice 2 sans tableau

--- TD1EXO2.java
+++ TD1EXO2.java
@@ -1,46 +1,27 @@
-import java.util.Scanner;
+
 
 class Calcul_Listes_Parfaits{
-    static boolean ifparfait(int x){
-        int cpt = 0;
-        int diviseurs[] = new int[10000];
-        int res = 0;
-        for(int i=1; i<= x/2; i++ ){
+    static void ifparfait(int x){
+    
+        String div = "1";
+        int res = 1;
+        for(int i=2; i<= x/2; i++ ){
             if(x%i == 0){
-                diviseurs[cpt] = i;
-                cpt++;
+                div = div + " + "  + i;
                 res = res + i;
             }
         }
         if(res == x){
-            System.out.print(x + "= ");
-            for(int i = 0; i< diviseurs.length ; i++){
-                if(diviseurs[i] == 0){
-                    break;
-                }
-                if(i == 0){
-                    System.out.print(diviseurs[i] + " ");
-                }
-                else if (i>0){
-                    System.out.print("+ " + diviseurs[i] + " ");
-                }
-                
-            }
-            System.out.print("\n");
-            return true;
+            System.out.println(x + "= " + div);
+            
         }
-        else{
-            return false;
-        }
-        
     }
 }
 
 public class TD1EXO2 {
     public static void main (String args[]) {
-        Scanner entree  =  new Scanner(System.in);
-
-        for(int i = 1; i<=100000; i++){
+       
+        for(int i = 2; i<=100000; i++){
             Calcul_Listes_Parfaits.ifparfait(i);
         }
     }
